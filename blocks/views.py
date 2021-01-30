d1cf16bb856c6edd784256cf04e8dac10f14b8a6
DimDimi4-and-fixiki <kalinindima123@yandex.ru>
2021-01-31 01:19:27 +0300

    Page navigation added

--- blocks/views.py
+++ blocks/views.py
@@ -14,8 +14,16 @@ def show_block_by_height(request, height: int):
 
 def index(request, page_num: int):
     response = api_handler.get_page(page_num=page_num, page_size=50)
+    prev_page = 0
+    if page_num == 1:
+        prev_page = "#"
+    else:
+        prev_page = page_num - 1
     context = {
         "data": response,
-        "page_num": page_num
+        "page_num": page_num,
+        "next_page_num": page_num + 1,
+        "prev_page_num": prev_page
+
     }
     return render(request, 'blocks/index.html', context=context)
