22098fc1e17bc96e7ac6fa44c2afe6dcf677203c
DimDimi4-and-fixiki <kalinindima123@yandex.ru>
2021-01-31 16:44:35 +0300

    Heroku initial

--- blockchain/settings.py
+++ blockchain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+*6)a4b!4_upm%2fx)n74^yylq6rx=glb37$wdexg(t7@^eyn4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ]
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
